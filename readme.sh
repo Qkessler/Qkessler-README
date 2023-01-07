d27ba1f3c6b1ed93f96bfd9fd10ba778bf423b74
Qkessler <qkessler@gmail.com>
2023-01-07 18:21:33 +0100

    Add git commit and git push on ./readme.sh file

--- readme.sh
+++ readme.sh
@@ -3,3 +3,10 @@
 export GH_ACCESS_TOKEN=$(pass show gh-access-token)
 
 ./generate-readme
+
+cd Qkessler
+git add .
+
+date=$(date '+%Y-%m-%d %H:%M:%S')
+git commit -am "Add dynamic README $date"
+git push
